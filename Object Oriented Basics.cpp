b42ed98e54162f1810014e91e53909cc67b087a5
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-05-01 10:55:28 +0530

    Create Object Oriented Basics.cpp

---
+++ Object Oriented Basics.cpp
@@ -0,0 +1,3 @@
+Object-oriented programming (OOP) is a style of programming that focuses on using objects to design and build applications.
+Objects: Objects represent a real-world entity and the basic building block of OOP. For example, an Online Shopping System will have objects
+such as shopping cart, customer, product item, etc.
